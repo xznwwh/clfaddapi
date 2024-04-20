15399ecf5373d81ef8a9e9c6e43b4729442c890d
Aomoda <528451599@qq.com>
2024-04-20 11:23:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,22 @@
 import requests
 
 def getAPI(url):
-
 	response = requests.get(url)
 	ip_addresses = []
 	idx = 1 
-	# 检查请求是否成功
 	if response.status_code == 200:
 	    json_data = response.json()
 	    for item in json_data['data']['good']:
 	    	ip_addresses.append(f"{item['ip']}#y{idx}")
-	    	idx = idx + 1
-	print(ip_addresses)    	
+	    	idx = idx + 1  	
 	return(ip_addresses)    	
 	    
-
-
 def w2d(dat):
 	csv_file_path = "./clfgoodip.csv"
 	with open(csv_file_path, 'w', newline='') as file:
-	    for ip in dat:
-	        file.write(ip + "\n")
-	print("SUCCESS")                
-
+	    for ip in dat[:10]:
+	        file.write(ip + "\n")               
 
 if __name__ == '__main__':
-	print("START")
 	api_url = "https://vps789.com/vps/sum/cfIpTop20"
 	w2d(getAPI(api_url))
