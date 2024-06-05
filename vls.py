624333ec251b3dc5ba1a9d3bd2427008af1143ab
Aomoda <528451599@qq.com>
2024-06-05 16:39:47 +0800

    Update vls.py

--- vls.py
+++ vls.py
@@ -1,4 +1,7 @@
 import requests
+import re
+from base64 import b64decode
+from urllib.parse import urlparse, parse_qs,unquote
 
 def getAPI(url):
 	response = requests.get(url)
@@ -6,25 +9,25 @@ def getAPI(url):
 	ips = []
 	idx = 1 
 	if response.status_code == 200:
-	    json_data = response.json()
-	    for item in json_data['outbounds']:
-	    	if item["type"]=="trojan":
-	    		tag = item["tag"]
-	    		server = item["server"]
+	    json_data = str(unquote(b64decode(response.content))).split("\n")
+	    for item in json_data:
+	    	rlt = extract_vless_info(item)
+	    	if len(rlt)>0:
+	    		server=rlt[0]
+	    		server_port=rlt[1]
 	    		if server in ips:
 	    			continue
-	    		server_port = item["server_port"]
-	    		if isselect(tag):
-	    			ips.append(server)
-	    			ip_addresses.append(f"{server}:{server_port}#HKG{idx}")
-	    			idx = idx + 1  	
+	    		ips.append(server)
+	    		ip_addresses.append(f"{server}:{server_port}#HKG{idx}")
+	    		idx = idx + 1  	
 	return(ip_addresses)
-	    
+
+
 def w2d(dat):
 	if len(dat) > 0 :
 		csv_file_path = "./myips.csv"
 		with open(csv_file_path, 'w', newline='') as file:
-		    for ip in dat[:10]:
+		    for ip in dat[:15]:
 		        file.write(str(ip).strip() + "\n")               
 
 
@@ -33,7 +36,21 @@ def isselect(tag):
 	for v in vls : 
 		if v in tag:
 			return(True)
+	return(False)		
+
+
+def extract_vless_info(vless_url):
+    pattern = re.compile(r"vless://[^@]+@([^:]+):(\d+).+#(.+)")
+    match = pattern.search(vless_url)
+    if match:
+        server_ip = match.group(1)
+        port = match.group(2)
+        name = match.group(3)
+        if isselect(name):
+        	return([server_ip, port])
+    return([])
+
 
 if __name__ == '__main__':
-	api_url = "https://ctjg.consi.site/hzSoC52WPgrv0Xon?singbox"
+	api_url = "https://alvless.filegear-sg.me/CMLiu"
 	w2d(getAPI(api_url))
